f74ff80366f7b36d41513962e1163d7f8acf43b2
Rickoroxd <0954744487zaz@gmail.com>
2024-12-26 22:26:10 +0700

    TUTORIAL

---
+++ effect.js
@@ -0,0 +1,19 @@
+document.write("Jakkrapong");
+/*Print IN JAVASCRIPT */ 
+
+alert("FUCK OFF");/*Show POPUP ใน หน้าเว็บ*/ 
+
+/*consloe จะแสดงให้ developer เห็นเท่านั้น*/ 
+console.log("WHAT THE HELL"); /*แสดงค่า สำหรับ debug */ 
+console.error("It HAVE ERROR");
+console.warn("Founnd Troubleshoot");
+
+
+/*VARIABLE in JAVASCRIPT*/
+
+//1.มี case Sensitive 
+//2. ขึ้นต้นด้วย $ _ ได้
+//3. ขึ้นต้น ด้วยตัวอักษร ENG เท่านั้น
+
+
+//JAVASCRIPT VARIABLE IS DYNAMIC TYPING (ไม่ต้องระบุชนิด)
